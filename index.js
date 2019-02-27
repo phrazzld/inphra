f68875138204b37912d4475620fba57f8b496578
Phaedrus <phraznikov@gmail.com>
2019-02-27 11:02:31 -0500

    To iterate is human; to recur, divine

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // index.js
 
 const exec = require('child_process').exec
+const fs = require('fs')
 
 // Test the cloning device
-const project = process.argv[2]
 const boilerplate = 'phrazzld/express-boilerplate.git'
 const gitAddr = `https://github.com/${boilerplate}`
 
@@ -13,14 +13,46 @@ const check = err => {
   }
 }
 
-const clone = exec(`git clone ${gitAddr} ${project}`, (err, stdout, stderr) => {
-  check(err)
-  console.log(stdout)
-  exec(`rm -rf ${project}/.git/ && cd ${project} && ls -la`, (err, stdout, stderr) => {
+const validatedProject = (project, count) => {
+  // Null validation
+  if (!project) {
+    return validatedProject('boiled-plates', count)
+  }
+  // Existing directory validation
+  if (fs.existsSync(`${project}/`)) {
+    count++
+    return validatedProject(`${project}-${count}`, count)
+  }
+  return project
+}
+const project = validatedProject(process.argv[2], 0)
+
+const deploy = cmds => {
+  const cmd = cmds.shift()
+  const proc = exec(cmd, (err, stdout, stderr) => {
     check(err)
     console.log(stdout)
+    if (cmds.length > 0) {
+      deploy(cmds)
+    }
   })
-})
-clone.on('exit', (code) => {
-  console.log(`\nExit code: ${code}\n`)
-})
+  proc.on('error', (error) => {
+    console.log(`\n${cmd}\nERROR: ${error}\n`)
+  })
+  proc.on('exit', (code) => {
+    console.log(`\n${cmd} (exit code ${code})\n`)
+  })
+}
+
+const commands = [
+  `git clone ${gitAddr} ${project}`,
+  `rm -rf ${project}/.git/`,
+  `sed -i '' 's/express-boilerplate/${project}/g' ${project}/package.json`,
+  `sed -i '' 's/express-boilerplate/${project}/g' ${project}/src/config.js`,
+  `sed -i '' 's/Express Boilerplate/${project}/g' ${project}/README.md`,
+  `sed -i '' 's/Express Boilerplate/${project}/g' ${project}/src/controllers/page.js`,
+  `sed -i '' 's/Express Boilerplate/${project}/g' ${project}/src/views/header.ejs`,
+  `cd ${project} && npm install`
+]
+
+deploy(commands)
